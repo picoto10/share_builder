272a99538a21b842965b95440a9bd4b220c26acb
picoto10 <83362034+picoto10@users.noreply.github.com>
2021-05-12 17:28:15 -0500

    20210512-17

--- function.js
+++ function.js
@@ -723,7 +723,7 @@ function codigos(componente){
 }
 
 function acortar(palabra){
-  if(palabra.length > 40){
+  if(palabra.length > 34){
     return palabra.substring(0,45)+"...";
   }else{
     return palabra+'<br>&nbsp;';
